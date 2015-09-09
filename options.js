23e6fa015d1a3b3ab3ce5270b322860f713032cf
Chad Woolley <pair+cwoolley@pivotal.io>
2015-09-08 23:13:38 -0700

    fix import race conditions and add failure alert, preserve import url, other fixes

--- options.js
+++ options.js
@@ -30,6 +30,12 @@ function save_options() {
 
 function restore_options() {
   chrome.storage.sync.get({
+    importUrl: "http://example.com/deploy_spy_config.json"
+  }, function(storage) {
+    $('#import_url').val(storage.importUrl);
+  });
+
+  chrome.storage.sync.get({
     storedEnvironments: {}
   }, function(items) {
     var index = 1;
@@ -46,6 +52,14 @@ function restore_options() {
   });
 }
 
+function clear_options() {
+  chrome.storage.sync.remove([
+    "storedEnvironments",
+    "importUrl"
+  ]);
+  restore_options();
+}
+
 function import_options() {
   // preserve existing values as long as they are not named
   // the same as an imported one
@@ -57,16 +71,22 @@ function import_options() {
   });
 
   var url = $('#import_url')[0].value;
+  chrome.storage.sync.set({importUrl: url});
 
-  $.getJSON(url, function(importedEnvironments) {
-    _.extend(environments, importedEnvironments);
-    chrome.storage.sync.set({storedEnvironments: environments});
-  });
-
-  restore_options();
+  $.getJSON(url)
+    .done(function(importedEnvironments) {
+      _.extend(environments, importedEnvironments);
+      chrome.storage.sync.set({storedEnvironments: environments});
+      restore_options();
+    })
+    .fail(function(f) {
+      alert('Import Failed (put a valid URL to .../deploy_spy_config.json)' + f.responseText);
+      return false;
+    });
   return false;
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('clear').addEventListener('click', clear_options);
 document.getElementById('import').addEventListener('click', import_options);
